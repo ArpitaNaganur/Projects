c0670f9187a497e22d686b8e513f6ce28b053476
Arpita Naganur <arpitanaganur@gmail.com>
2022-01-30 22:11:32 -0500

    Adding Expense Tracker

---
+++ Expense Tracker/src/componenets/NewExpense/ExpenseForm.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmt, setEnteredAmt] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const amtChangeHandler = (event) => {
+        setEnteredAmt(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        
+    }
+    const submitHandler = (event) =>{
+        event.preventDefault();
+        const expenseData = {
+            name: enteredTitle,
+            amount: enteredAmt,
+            date: new Date(enteredDate)
+        }
+        props.onSaveExpense();
+        setEnteredTitle('');
+        setEnteredAmt('');
+        setEnteredDate('');
+        
+    }
+    return <form id="expenseForm" onSubmit={submitHandler}>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
+            </div >
+            <div className='new-expense__control'>
+                <label>Amount</label>
+                <input type='number' min='0.1' step='0.01' value={enteredAmt} onChange={amtChangeHandler}/>
+            </div>
+            <div className='new-expense__control'>
+                <label>Date</label>
+                <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler}/>
+            </div>
+        </div>
+        <div className='new-expense__actions'>
+            <button type='submit'>Add Expense</button>
+        </div>
+    </form>
+
+}
+
+export default ExpenseForm;
